9d7f117d02e94555fc7b99079b7d8fe2eae16980
JH <wasicrob27@gmail.com>
2022-08-02 22:14:15 +0900

    Add Item entity & repo

---
+++ src/main/java/com/clone/ohouse/shop/order/domain/entity/Item.java
@@ -0,0 +1,49 @@
+package com.clone.ohouse.shop.order.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long itemSeq;
+
+    @ManyToOne
+    @JoinColumn(name = "category_code")
+    private ItemCategoryCode categoryCode;
+
+    @Column(length = 45, nullable = false)
+    private String name;
+
+    @Column(length = 50)
+    private String modelName;
+
+    @Column(length = 45)
+    private String brandName;
+
+
+    @Builder
+    public Item(ItemCategoryCode categoryCode, String name, String modelName, String brandName) {
+        this.categoryCode = categoryCode;
+        this.name = name;
+        this.modelName = modelName;
+        this.brandName = brandName;
+    }
+
+    public void update(String name, String modelName, String brandName){
+        this.name = name;
+        this.modelName = modelName;
+        this.brandName = brandName;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
