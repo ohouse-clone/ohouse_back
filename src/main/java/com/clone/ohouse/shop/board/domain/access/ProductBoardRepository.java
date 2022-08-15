7c6ed2e262b3143e808db44a8b037040faeec740
JeongHeon <wasicrob27@gmail.com>
2022-08-15 23:21:10 +0900

    Fix Annotation in ProductBoardRepository

--- src/main/java/com/clone/ohouse/shop/board/domain/access/ProductBoardRepository.java
+++ src/main/java/com/clone/ohouse/shop/board/domain/access/ProductBoardRepository.java
@@ -2,6 +2,8 @@ package com.clone.ohouse.shop.board.domain.access;
 
 import com.clone.ohouse.shop.board.domain.entity.ProductBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductBoardRepository extends JpaRepository<ProductBoard, Long> {
 }
