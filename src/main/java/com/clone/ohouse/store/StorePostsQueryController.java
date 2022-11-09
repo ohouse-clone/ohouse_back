b5d91d7b17fa416f8f3fa01cece9e4129a35487b
JH <wasicrob27@gmail.com>
2022-11-09 13:15:54 +0900

    feat: Add getBundleView Controller 1

--- src/main/java/com/clone/ohouse/store/StorePostsQueryController.java
+++ src/main/java/com/clone/ohouse/store/StorePostsQueryController.java
@@ -1,7 +1,54 @@
 package com.clone.ohouse.store;
 
+import com.clone.ohouse.store.domain.StorePostsQueryService;
+import com.clone.ohouse.store.domain.category.Category;
+import com.clone.ohouse.store.domain.category.CategoryRepository;
+import com.clone.ohouse.store.domain.category.CategorySearch;
+import com.clone.ohouse.store.domain.item.itemselector.ItemSelector;
+import com.clone.ohouse.store.domain.storeposts.dto.BundleVIewDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @RestController
 public class StorePostsQueryController {
+
+    private StorePostsQueryService storePostsQueryService;
+    private CategoryRepository categoryRepository;
+
+    @PostMapping("/store/category")
+    public BundleVIewDto getBundleView(@RequestParam MultiValueMap<String, Object> paramMap, Pageable pageable) throws Exception{
+        List<Object> categories = paramMap.get("category");
+        if(categories.size() <= 0) return null;
+        CategorySearch categorySearch = parseCategoryString(categories);
+
+        Category category = categoryRepository.findCategory(categorySearch);
+        Optional<Class> type = new ItemSelector().selectTypeFrom(category.getName());
+        Class classType = type.get();
+        if(classType == )
+
+
+        storePostsQueryService.getBundleViewV3(categorySearch, pageable, )
+    }
+
+    private CategorySearch parseCategoryString(List<Object> category) {
+        CategorySearch categorySearch = new CategorySearch(null, null, null, null);
+
+        String categoryCodes = (String) category.get(0);
+        String[] s = categoryCodes.split("_");
+
+        if(s.length >= 1) categorySearch.setCode4(Long.parseLong(s[0]));
+        if(s.length >= 2) categorySearch.setCode4(Long.parseLong(s[1]));
+        if(s.length >= 3) categorySearch.setCode4(Long.parseLong(s[2]));
+        if(s.length >= 4) categorySearch.setCode4(Long.parseLong(s[3]));
+        return categorySearch;
+    }
+
 }
