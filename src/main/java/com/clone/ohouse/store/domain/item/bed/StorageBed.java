c5888d8884b7be681852a878ef06f66b3a170101
JH <wasicrob27@gmail.com>
2022-11-09 10:36:54 +0900

    feat: Add StorageBed condition checking

--- src/main/java/com/clone/ohouse/store/domain/item/bed/StorageBed.java
+++ src/main/java/com/clone/ohouse/store/domain/item/bed/StorageBed.java
@@ -1,6 +1,7 @@
 package com.clone.ohouse.store.domain.item.bed;
 
 import com.clone.ohouse.store.domain.item.Item;
+import org.springframework.web.bind.annotation.MatrixVariable;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -11,5 +12,14 @@ public class StorageBed extends Item {
     @Enumerated(value = EnumType.STRING)
     private Material material;
 
+    public StorageBed(String name, String modelName, String brandName, Material material) {
+        super(name, modelName, brandName);
+        this.material = material;
+    }
+
+    public void update(String name, String modelName, String brandName, Material material) {
+        super.update(name, modelName, brandName);
+        if(material != null) this.material = material;
+    }
 
 }
