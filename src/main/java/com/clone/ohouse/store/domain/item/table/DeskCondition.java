dc7c9bd216b3a25491abd37af8704236d29d6947
JeongHeon <wasicrob27@gmail.com>
2023-01-22 11:31:31 +0900

    feat: search condition 1

---
+++ src/main/java/com/clone/ohouse/store/domain/item/table/DeskCondition.java
@@ -0,0 +1,12 @@
+package com.clone.ohouse.store.domain.item.table;
+
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+public class DeskCondition {
+    public DeskColor[] deskColors = new DeskColor[3];
+
+    public BooleanExpression eqDeskCondition(){
+
+    }
+}
